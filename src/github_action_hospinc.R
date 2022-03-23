aaf37679cf782b956fd1aae649e128d418d9f782
whoohoo86 <70888570+whoohoo86@users.noreply.github.com>
2022-03-23 10:21:17 +0100

    Update github_action_hospinc.R

--- src/github_action_hospinc.R
+++ src/github_action_hospinc.R
@@ -22,4 +22,4 @@ two_bold <- createStyle(textDecoration = "bold",  numFmt = "0.00")
 addStyle(wb, 1, style = two, rows = 4:19, cols = 2:63, gridExpand = TRUE)
 addStyle(wb, 1, style = two_bold, rows = 20, cols = 1:63, gridExpand = TRUE)
 
-saveWorkbook(wb, paste0("7-Tage-Inzidenzen_",Sys.Date(),".xlsx"), overwrite = TRUE)
+saveWorkbook(wb, paste0("./data/7-Tage-Inzidenzen_",Sys.Date(),".xlsx"), overwrite = TRUE)
